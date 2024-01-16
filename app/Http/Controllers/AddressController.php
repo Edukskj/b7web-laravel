cb641abb89b4c0e3e02e265823fc3ef262600a75
Edukskj <trindadev494@gmail.com>
2024-01-16 16:12:05 -0300

    "Término da criação das funções e tabelas"

---
+++ app/Http/Controllers/AddressController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Address;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AddressController extends Controller
+{
+    public function index() {
+        $addresses = Address::all();
+        return $addresses;
+    }
+
+    public function findOne(Request $request) {
+        $address = Address::find($request->id);
+        return $address;
+    }
+
+    public function create() {
+        $info = [
+            'address' => 'Rua dos Monjueiros'
+        ];
+        $address = new Address($info);
+        $address->save();
+
+        return $address;
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index ()
+    {
+        $users = User::all();
+        return $users;
+    }
+
+    public function findOne(Request $request) {
+        $user = User::find($request->id);
+        return $user;
+    }
+
+    public function create() {
+        $info = [
+            'name' => 'Joaozin',
+            'email' => 'joao@hotmail.com',
+            'password' => 'jaojao'
+        ];
+        $user = new User($info);
+        $user->save();
+
+        return $user;
+    }
+}
